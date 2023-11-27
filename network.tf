00801a36c0be945746d84f996689316c3cec2e0b
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-27 16:18:42 -0600

    modified:   network.tf

--- network.tf
+++ network.tf
@@ -64,18 +64,6 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "Allow-HTTPS"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "Allow-SSH"
     priority                   = 102
     direction                  = "Inbound"
