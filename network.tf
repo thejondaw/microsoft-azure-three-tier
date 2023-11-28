36bd69e3345a5cace327c95040c86fcde8a86905
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-28 00:37:09 -0600

    modified:   network.tf

--- network.tf
+++ network.tf
@@ -97,7 +97,7 @@ resource "azurerm_subnet_network_security_group_association" "nsg-sub" {
 resource "azurerm_firewall" "example" {
   name                = "firewall"
   location            = var.location
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.azure-project.name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 
