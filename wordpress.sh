1e6a55479514595d5c5fde03ba83c05cebf3f51d
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-29 14:19:35 -0600

    modified:   wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,59 +1,37 @@
 !/bin/bash
-# install-wordpress-centos8.sh
-#
-# Description: This script will download, configure and install WordPress for CentOS/RHEL 8.x Linux.
-#
 
-# Open firewall ports
-firewall-cmd --permanent --add-service={http,https}
-firewall-cmd --reload
+# Disable SELinux
+sudo getenforce
+sudo setenforce 0
 
-dnf module enable php:8.0 -y
+# Install Apache
+sudo yum install httpd mysql -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
-# Install the database
-dnf -y install mariadb-server httpd php php-mysqlnd dos2unix php-gd php-mbstring php-json
-systemctl enable --now mariadb
+# Install WGET and Unzip
+sudo yum install wget unzip -y
 
-# Add to the database
-echo 'CREATE DATABASE wordpress;' | mysql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'password';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
+# Download and extract website template
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/finexo.zip
+unzip finexo.zip
+mv finexo-html/* /var/www/html/
 
-# Download and install WordPress
-mkdir -p /var/www/html/wordpress
-curl -O https://wordpress.org/latest.tar.gz
-tar -C /var/www/html/wordpress --strip-components=1 -zxvf latest.tar.gz && rm -f latest.tar.gz
+# Install PHP 7.3
+sudo yum install epel-release yum-utils -y
+sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+sudo yum-config-manager --enable remi-php73
+sudo yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
+sudo systemctl restart httpd
 
-cd /var/www/html/wordpress
-mkdir /var/www/html/wordpress/wp-content/{uploads,cache}
-chown apache:apache /var/www/html/wordpress/wp-content/{uploads,cache}
+# Install Wordpress
+sudo wget https://en-gb.wordpress.org/latest-en_GB.tar.gz
+sudo tar -xf latest-en_GB.tar.gz -C /var/www/html/
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo chown -R apache:apache /var/www/html/
 
-# Configure WordPress
-cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sed -i 's@database_name_here@project-db@' /var/www/html/wordpress/wp-config.php
-sed -i 's@username_here@adminuser@' /var/www/html/wordpress/wp-config.php
-sed -i 's@password_here@pa$$w0rd@' /var/www/html/wordpress/wp-config.php
-curl https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/html/wordpress/wp-config.php
-
-# Modify the .htaccess
-cat << 'EOF' >> /var/www/html/wordpress/.htaccess
-# BEGIN WordPress
-
-   RewriteEngine On
-   RewriteBase /
-   RewriteRule ^index\.php$ - [L]
-   RewriteCond %{REQUEST_FILENAME} !-f
-   RewriteCond %{REQUEST_FILENAME} !-d
-   RewriteRule . /index.php [L]
-
-# END WordPress"
-EOF
-chmod 666 /var/www/html/wordpress/.htaccess
-
-# Configure and start Apache
-sed -i "/^/,/^<\/Directory>/{s/AllowOverride None/AllowOverride All/g}" /etc/httpd/conf/httpd.conf
-systemctl enable --now httpd
-
-# there are problems with the wp-config.php, convert to unix
-dos2unix /var/www/html/wordpress/wp-config.php
-chown apache:apache -R /var/www/html/wordpress
+# Set-Up Database credentials
+sudo sed -i 's/database_name_here/project-db/g' /var/www/html/wp-config.php
+sudo sed -i 's/username_here/adminuser@project-mysql-server/g' /var/www/html/wp-config.php
+sudo sed -i 's/password_here/pa$$w0rd/g' /var/www/html/wp-config.php
+sudo sed -i 's/localhost/project-mysql-server.mysql.database.azure.com/g' /var/www/html/wp-config.php
