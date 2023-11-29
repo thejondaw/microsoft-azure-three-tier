1ac7594e206773797b924325ecd23a4643499a29
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-29 01:25:21 -0600

    modified:   wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -39,10 +39,15 @@ sudo yum-config-manager --enable remi-php73
 sudo yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
 sudo systemctl restart httpd
 
-
+# Install Wordpress
+sudo wget https://en-gb.wordpress.org/latest-en_GB.tar.gz
+sudo tar -xf latest-en_GB.tar.gz -C /var/www/html/
+sudo mv /var/www/html/wordpress/* /var/www/html/
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo chown -R apache:apache /var/www/html/
 
 # Set-Up Database credentials
-sudo sed 's/database_name_here/project-db/g' /var/www/html/wp-config.php -i
-sudo sed 's/username_here/adminuser@project-mysql-server/g' /var/www/html/wp-config.php -i
-sudo sed 's/password_here/pa$$w0rd/g' /var/www/html/wp-config.php -i
-sudo sed 's/localhost/project-mysql-server.mysql.database.azure.com/g' /var/www/html/wp-config.php -i
+sudo sed 's/project-db/g' /var/www/html/wp-config.php -i
+sudo sed 's/adminuser@project-mysql-server/g' /var/www/html/wp-config.php -i
+sudo sed 's/pa$$w0rd/g' /var/www/html/wp-config.php -i
+sudo sed 's/project-mysql-server.mysql.database.azure.com/g' /var/www/html/wp-config.php -i
