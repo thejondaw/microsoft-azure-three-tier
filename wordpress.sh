fd114d40e99d031ecbb9b77076edc4b32b41aa83
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-28 23:59:25 -0600

    modified:   wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
 
-# Install and configure Apache
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
+# Disable SELinux
+sudo getenforce
+sudo sed 's/SELINUX=permissive/SELINUX=enforcing/g' /etc/sysconfig/selinux -i
+sudo setenforce 0
 
-# Install WGET and Unzip
-yum install wget unzip -y
-
-# Install and configure PHP 7.3
-yum install epel-release yum-utils -y
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
-yum-config-manager --enable remi-php73
-yum install php php-mysqli -y
-systemctl restart httpd
-
-# Install WordPress
-wget https://en-gb.wordpress.org/latest-en_GB.tar.gz
-tar -xf latest-en_GB.tar.gz
-rm -rf /var/www/html/*
-mv wordpress/* /var/www/html/
-chown -R apache:apache /var/www/html
-
-# Download and extract website template
-wget https://www.free-css.com/assets/files/free-css-templates/download/page296/finexo.zip
-unzip finexo.zip
-mv finexo-html/* /var/www/html/
-setenforce 0
+# Install Apache
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 # Install and configure MariaDB
-yum install mariadb mariadb-server -y
-systemctl start mariadb
-systemctl enable mariadb
+sudo yum install mariadb mariadb-server -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
 
 # Secure MariaDB installation
 mysql_secure_installation <<EOF
@@ -41,3 +23,31 @@ y
 y
 y
 EOF
+
+# Install PHP 7.3
+sudo yum install epel-release yum-utils -y
+sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+sudo yum-config-manager --enable remi-php73
+sudo yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo -y
+sudo systemctl restart httpd
+
+# Install WGET and Unzip
+sudo yum install wget unzip -y
+
+# Download and extract website template
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/finexo.zip
+unzip finexo.zip
+mv finexo-html/* /var/www/html/
+
+# Install Wordpress
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xf latest.tar.gz -C /var/www/html/
+sudo mv /var/www/html/wordpress/* /var/www/html/
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo chown -R apache:apache /var/www/html/
+
+# Set-Up Database credentials
+sudo sed 's/database_name_here/project-db/g' /var/www/html/wp-config.php -i
+sudo sed 's/username_here/adminuser@project-mysql-server/g' /var/www/html/wp-config.php -i
+sudo sed 's/password_here/pa$$w0rd/g' /var/www/html/wp-config.php -i
+sudo sed 's/localhost/project-mysql-server.mysql.database.azure.com/g' /var/www/html/wp-config.php -i
