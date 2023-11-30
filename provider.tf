c56dd454f9467437df2bf8da70d55fee7e0ded09
mrjondaw@gmail.com <mrjondaw@gmail.com>
2023-11-30 16:38:01 -0600

    modified:   provider.tf

--- provider.tf
+++ provider.tf
@@ -19,14 +19,14 @@ provider "azurerm" {
 # Configure the AWS account
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAWYTMUTGENK5WHRMI"
+  secret_key = "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"
 }
 
 # AWS Route 53
 resource "aws_route53_record" "wordpressdb" {
-  zone_id = ""
-  name    = "wordpress..net"
+  zone_id = "Z01550132JHOXO5DE6BV5"
+  name    = "wordpress.pathofneo.net"
   type    = "A"
   ttl     = 300
   records = [azurerm_public_ip.example.ip_address]
